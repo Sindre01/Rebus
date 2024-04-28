6d70e04119d990b6d7c7073226eb144b980353a5
Sindre Rotnes Erichsen <sindrre@ulrik.uio.no>
2024-04-28 15:25:52 +0200

    Extracted presentation services into seperate builder

--- Rebus.API/Program.cs
+++ Rebus.API/Program.cs
@@ -1,47 +1,19 @@
-
-using Microsoft.OpenApi.Models;
+using Rebus.API.Extensions;
 using Rebus.API.Middlewares;
 using Rebus.Application.Extensions;
 using Rebus.Domain.Entities;
 using Rebus.Infrastructure.Extensions;
 using Rebus.Infrastructure.Seeders;
 using Serilog;
-using Serilog.Events;
 
 var builder  = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGen(c =>
-    {
-        c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
-        {
-            Type= SecuritySchemeType.Http,
-            Scheme= "Bearer"
-        });
-
-        c.AddSecurityRequirement(new OpenApiSecurityRequirement
-        {
-            {
-                new OpenApiSecurityScheme
-                {
-                    Reference = new OpenApiReference{ Type = ReferenceType.SecurityScheme, Id = "bearerAuth"}
-                },
-                []
-            }
-        });
-    });
-
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddScoped<ErrorHandlingMiddleware>();
+builder.AddPresentation();
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
-builder.Host.UseSerilog((context, configuration) =>
-    configuration.ReadFrom.Configuration(context.Configuration)
-);
 
 var app = builder.Build();
 
